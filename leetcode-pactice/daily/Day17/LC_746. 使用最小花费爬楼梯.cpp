1d0d01a0973f25e2971d520dcfdda1459bf06422
bytes <2057335724@qq.com>
2025-12-16 20:30:35 +0800

    Add pactice of Day17

---
+++ leetcode-pactice/daily/Day17/LC_746. 使用最小花费爬楼梯.cpp
@@ -0,0 +1,18 @@
+/* 
+    url:https://leetcode.cn/problems/min-cost-climbing-stairs/description/
+    difficulty:easy
+    tags:dp
+*/
+
+class Solution {
+public:
+    int minCostClimbingStairs(vector<int>& cost) {
+        vector<int> dp(cost.size()+1);
+        dp[0] = 0;
+        dp[1] = 0;
+        for(int i=2; i<=cost.size(); i++){
+            dp[i] = min(dp[i-1]+cost[i-1],dp[i-2]+cost[i-2]);
+        }
+        return dp.back();
+    }
+};
